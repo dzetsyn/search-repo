7780a0027f3359934fa619d98b4a70faaa30f1db
dzetsyn <dmitry@sanjose.su>
2020-06-08 00:37:43 -0700

    added watchers counter + icon

--- src/components/RepoItem.js
+++ src/components/RepoItem.js
@@ -3,12 +3,14 @@ import { Typography, Tag } from 'antd';
 import dayjs from 'dayjs';
 import numeral from 'numeral';
 import { ReactComponent as StarIcon } from './star.svg';
+import { ReactComponent as WatchIcon } from './watch.svg';
 
 const { Title } = Typography;
 
 const RepoItem = props => {
     const updatedDateTime = dayjs(props.updated_at).format('MMM D, YYYY');
-    const stars = props.stargazers_count ? numeral(props.stargazers_count).format('0,0') : 0;
+    const stars = props.stargazers_count ? numeral(props.stargazers_count).format('0,0') : null;
+    const watchers = props.watchers_count ? numeral(props.watchers_count).format('0,0') : null
 
     return (
         <div className="site-layout-background" style={{ padding: 20 }}>
@@ -21,14 +23,27 @@ const RepoItem = props => {
 
             <div style={{ display: 'flex', marginTop: 10 }}>
                 {/* // STARS */}
-                <span style={{ marginRight: 10, display: 'flex', alignItems: 'center' }}>
-                    <StarIcon />&nbsp;{stars}
-                </span>
+                {
+                    stars ?
+                        <span style={{ marginRight: 15, display: 'flex', alignItems: 'center' }}>
+                            <StarIcon />&nbsp;{stars}
+                        </span>
+                        : <div />
+                }
 
                 {/* // LANGUAGE BADGE */}
-                <span style={{ marginRight: 10 }}>
+                <span style={{ marginRight: 15 }}>
                     <Tag color="green">{props.language}</Tag>
                 </span>
+
+                {
+                    watchers ?
+                        <span style={{ marginRight: 15, display: 'flex', alignItems: 'center' }}>
+                            <WatchIcon />&nbsp;{watchers}
+                        </span>
+                        :
+                        <div />
+                }
 
                 {/* // UPDATED */}
                 <span style={{ fontSize: 12, display: 'flex', alignItems: 'center' }}>
