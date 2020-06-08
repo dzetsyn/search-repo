89a9dc8ff06acede99ed3c5fe532fce0ac0d17ca
dzetsyn <dmitry@sanjose.su>
2020-06-08 00:04:48 -0700

    updates

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-// ACTIONS
-
-export const repoSearch = props => ({
-    type: 'REPO_SEARCH',
-    itemsPerPage: props.itemsPerPage,
-    currentPage: props.currentPage,
-    text: props.text,
-    language: props.language
-});
